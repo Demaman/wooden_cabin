68fc355a4158aae2c1d835b099f812f173b2be2c
Perion Demaman <perion.f.demaman@accenture.com>
2025-07-30 22:45:58 -0300

    layout.tsx for deploying

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+
+import type { Metadata } from 'next'; // Optional: for global metadata
+
+import './globals.css';
+export const metadata: Metadata = {
+  title: 'Vita Cabanas',
+  description: 'Your beautiful life in the mountains of Nova Trento SC',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR"> {/* Set your default language here, or 'en' */}
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
